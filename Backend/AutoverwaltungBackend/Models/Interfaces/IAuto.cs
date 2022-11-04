457c7c41db1792a0f1fae64e96757ef7e8e4304d
Danny Held <d.held@focus-ia.de>
2022-11-04 11:47:06 +0100

    added Interfaces

---
+++ Backend/AutoverwaltungBackend/Models/Interfaces/IAuto.cs
@@ -0,0 +1,14 @@
+﻿namespace AutoverwaltungBackend.Models.Interfaces
+{
+    public interface IAuto
+    {
+         short autoId { get; set; }
+         string marke { get; set; }
+         string model { get; set; }
+         short baujahr { get; set; }
+         float kmStand { get; set; }
+         short ps { get; set; }
+         string kennzeichen { get; set; }
+         AutoStatus autoStatus { get; set; }
+    }
+}
