830944d6509115b7db43af0722431a5d48bfc810
lucutz5 <luca.schmitz@gmx.de>
2022-11-04 11:55:15 +0100

    AutoStatusController erstellt

---
+++ Backend/AutoverwaltungBackend/Controllers/AutoStatusController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoverwaltungBackend.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class AutoStatusController : ControllerBase
+    {
+        [HttpGet]
+        [Route("[controller]/{id}")]
+        public ActionResult<AutoStatus> getAutoStatus(short id)
+        {
+
+            return new OkObjectResult(new AutoStatus());
+        }
+        [HttpPatch]
+        [Route("[controller]/{id}")]
+        public ActionResult<AutoStatus> updateAutoStatus(short id)
+        {
+
+            return new OkObjectResult(new AutoStatus());
+        }
+    }
+}
